8479bf20f9bb9205c97afcb0b5afaba13e57186f
ulyweb <uly@ulynetwork.com>
2025-04-07 09:53:53 -0700

    Create Get-InstalledSoftware.ps1

---
+++ scripts/Get-InstalledSoftware.ps1
@@ -0,0 +1,18 @@
+Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | 
+  Where-Object { $_.DisplayName -like "*Microsoft*" } | 
+  Select-Object DisplayName, DisplayVersion
+
+
+
+function Get-InstalledSoftware {
+    # Prompt the user for input
+    $searchString = Read-Host "Please enter the string to search for in DisplayName"
+
+    # Retrieve and filter registry values based on user input
+    Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" |
+        Where-Object { $_.DisplayName -like "*$searchString*" } |
+        Select-Object DisplayName, DisplayVersion
+}
+
+# Example usage
+Get-InstalledSoftware
