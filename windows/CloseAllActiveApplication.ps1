602fcae638132548b388114b483dc03419830650
ulyweb <uly@ulynetwork.com>
2025-11-24 09:43:58 -0800

    Update CloseAllActiveApplication.ps1

--- windows/CloseAllActiveApplication.ps1
+++ windows/CloseAllActiveApplication.ps1
@@ -19,11 +19,6 @@ $CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
 $Principal = New-Object Security.Principal.WindowsPrincipal $CurrentUser
 $IsAdmin = $Principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-if (-not $IsAdmin) {
-    Write-Host "This script must be run as an Administrator. Restarting with elevated privileges..."
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    exit
-}
 
 # --- Configuration (Exclusions) ---
 # 1. Define processes that should NEVER be terminated, even if they have a main window.
